092eb1d2b7e336de161ad2b17b5097aab61b4715
wklos <wklos@captechventures.com>
2018-10-21 20:05:50 -0400

    Removed vendor tracking test.

--- gettysburg.sh
+++ gettysburg.sh
@@ -1,271 +1,542 @@
 curl -X POST http://localhost:7718/add/Four
+printf "\n" 
 curl -X POST http://localhost:7718/add/score
+printf "\n"
 curl -X POST http://localhost:7718/add/and
+printf "\n"
 curl -X POST http://localhost:7718/add/seven
+printf "\n"
 curl -X POST http://localhost:7718/add/years
+printf "\n"
 curl -X POST http://localhost:7718/add/ago
+printf "\n"
 curl -X POST http://localhost:7718/add/our
+printf "\n"
 curl -X POST http://localhost:7718/add/fathers
+printf "\n"
 curl -X POST http://localhost:7718/add/brought
+printf "\n"
 curl -X POST http://localhost:7718/add/forth
+printf "\n"
 curl -X POST http://localhost:7718/add/on
+printf "\n"
 curl -X POST http://localhost:7718/add/this
+printf "\n"
 curl -X POST http://localhost:7718/add/continent
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/new
+printf "\n"
 curl -X POST http://localhost:7718/add/nation
+printf "\n"
 curl -X POST http://localhost:7718/add/conceived
+printf "\n"
 curl -X POST http://localhost:7718/add/in
+printf "\n"
 curl -X POST http://localhost:7718/add/Liberty
+printf "\n"
 curl -X POST http://localhost:7718/add/and
+printf "\n"
 curl -X POST http://localhost:7718/add/dedicated
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/proposition
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/all
+printf "\n"
 curl -X POST http://localhost:7718/add/men
+printf "\n"
 curl -X POST http://localhost:7718/add/are
+printf "\n"
 curl -X POST http://localhost:7718/add/created
+printf "\n"
 curl -X POST http://localhost:7718/add/equal
+printf "\n"
 curl -X POST http://localhost:7718/add/Now
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/are
+printf "\n"
 curl -X POST http://localhost:7718/add/engaged
+printf "\n"
 curl -X POST http://localhost:7718/add/in
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/great
+printf "\n"
 curl -X POST http://localhost:7718/add/civil
+printf "\n"
 curl -X POST http://localhost:7718/add/war
+printf "\n"
 curl -X POST http://localhost:7718/add/testing
+printf "\n"
 curl -X POST http://localhost:7718/add/whether
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/nation
+printf "\n"
 curl -X POST http://localhost:7718/add/or
+printf "\n"
 curl -X POST http://localhost:7718/add/any
+printf "\n"
 curl -X POST http://localhost:7718/add/nation
+printf "\n"
 curl -X POST http://localhost:7718/add/so
+printf "\n"
 curl -X POST http://localhost:7718/add/conceived
+printf "\n"
 curl -X POST http://localhost:7718/add/and
+printf "\n"
 curl -X POST http://localhost:7718/add/so
+printf "\n"
 curl -X POST http://localhost:7718/add/dedicated
+printf "\n"
 curl -X POST http://localhost:7718/add/can
+printf "\n"
 curl -X POST http://localhost:7718/add/long
+printf "\n"
 curl -X POST http://localhost:7718/add/endure
+printf "\n"
 curl -X POST http://localhost:7718/add/We
+printf "\n"
 curl -X POST http://localhost:7718/add/are
+printf "\n"
 curl -X POST http://localhost:7718/add/met
+printf "\n"
 curl -X POST http://localhost:7718/add/on
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/great
+printf "\n"
 curl -X POST http://localhost:7718/add/battlefield
+printf "\n"
 curl -X POST http://localhost:7718/add/of
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/war
+printf "\n"
 curl -X POST http://localhost:7718/add/We
+printf "\n"
 curl -X POST http://localhost:7718/add/have
+printf "\n"
 curl -X POST http://localhost:7718/add/come
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/dedicate
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/portion
+printf "\n"
 curl -X POST http://localhost:7718/add/of
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/field
+printf "\n"
 curl -X POST http://localhost:7718/add/as
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/final
+printf "\n"
 curl -X POST http://localhost:7718/add/resting
+printf "\n"
 curl -X POST http://localhost:7718/add/place
+printf "\n"
 curl -X POST http://localhost:7718/add/for
+printf "\n"
 curl -X POST http://localhost:7718/add/those
+printf "\n"
 curl -X POST http://localhost:7718/add/who
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/gave
+printf "\n"
 curl -X POST http://localhost:7718/add/their
+printf "\n"
 curl -X POST http://localhost:7718/add/lives
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/nation
+printf "\n"
 curl -X POST http://localhost:7718/add/might
+printf "\n"
 curl -X POST http://localhost:7718/add/live
+printf "\n"
 curl -X POST http://localhost:7718/add/It
+printf "\n"
 curl -X POST http://localhost:7718/add/is
+printf "\n"
 curl -X POST http://localhost:7718/add/altogether
+printf "\n"
 curl -X POST http://localhost:7718/add/fitting
+printf "\n"
 curl -X POST http://localhost:7718/add/and
+printf "\n"
 curl -X POST http://localhost:7718/add/proper
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/should
+printf "\n"
 curl -X POST http://localhost:7718/add/do
+printf "\n"
 curl -X POST http://localhost:7718/add/this
+printf "\n"
 curl -X POST http://localhost:7718/add/But
+printf "\n"
 curl -X POST http://localhost:7718/add/in
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/larger
+printf "\n"
 curl -X POST http://localhost:7718/add/sense
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/can
+printf "\n"
 curl -X POST http://localhost:7718/add/not
+printf "\n"
 curl -X POST http://localhost:7718/add/dedicate
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/can
+printf "\n"
 curl -X POST http://localhost:7718/add/not
+printf "\n"
 curl -X POST http://localhost:7718/add/consecrate
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/can
+printf "\n"
 curl -X POST http://localhost:7718/add/not
+printf "\n"
 curl -X POST http://localhost:7718/add/hallow
+printf "\n"
 curl -X POST http://localhost:7718/add/this
+printf "\n"
 curl -X POST http://localhost:7718/add/ground
+printf "\n"
 curl -X POST http://localhost:7718/add/The
+printf "\n"
 curl -X POST http://localhost:7718/add/brave
+printf "\n"
 curl -X POST http://localhost:7718/add/men
+printf "\n"
 curl -X POST http://localhost:7718/add/living
+printf "\n"
 curl -X POST http://localhost:7718/add/and
+printf "\n"
 curl -X POST http://localhost:7718/add/dead
+printf "\n"
 curl -X POST http://localhost:7718/add/who
+printf "\n"
 curl -X POST http://localhost:7718/add/struggled
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/have
+printf "\n"
 curl -X POST http://localhost:7718/add/consecrated
+printf "\n"
 curl -X POST http://localhost:7718/add/it
+printf "\n"
 curl -X POST http://localhost:7718/add/far
+printf "\n"
 curl -X POST http://localhost:7718/add/above
+printf "\n"
 curl -X POST http://localhost:7718/add/our
+printf "\n"
 curl -X POST http://localhost:7718/add/poor
+printf "\n"
 curl -X POST http://localhost:7718/add/power
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/add
+printf "\n"
 curl -X POST http://localhost:7718/add/or
+printf "\n"
 curl -X POST http://localhost:7718/add/detract
+printf "\n"
 curl -X POST http://localhost:7718/add/The
+printf "\n"
 curl -X POST http://localhost:7718/add/world
+printf "\n"
 curl -X POST http://localhost:7718/add/will
+printf "\n"
 curl -X POST http://localhost:7718/add/little
+printf "\n"
 curl -X POST http://localhost:7718/add/note
+printf "\n"
 curl -X POST http://localhost:7718/add/nor
+printf "\n"
 curl -X POST http://localhost:7718/add/long
+printf "\n"
 curl -X POST http://localhost:7718/add/remember
+printf "\n"
 curl -X POST http://localhost:7718/add/what
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/say
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/but
+printf "\n"
 curl -X POST http://localhost:7718/add/it
+printf "\n"
 curl -X POST http://localhost:7718/add/can
+printf "\n"
 curl -X POST http://localhost:7718/add/never
+printf "\n"
 curl -X POST http://localhost:7718/add/forget
+printf "\n"
 curl -X POST http://localhost:7718/add/what
+printf "\n"
 curl -X POST http://localhost:7718/add/they
+printf "\n"
 curl -X POST http://localhost:7718/add/did
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/It
+printf "\n"
 curl -X POST http://localhost:7718/add/is
+printf "\n"
 curl -X POST http://localhost:7718/add/for
+printf "\n"
 curl -X POST http://localhost:7718/add/us
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/living
+printf "\n"
 curl -X POST http://localhost:7718/add/rather
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/be
+printf "\n"
 curl -X POST http://localhost:7718/add/dedicated
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/unfinished
+printf "\n"
 curl -X POST http://localhost:7718/add/work
+printf "\n"
 curl -X POST http://localhost:7718/add/which
+printf "\n"
 curl -X POST http://localhost:7718/add/they
+printf "\n"
 curl -X POST http://localhost:7718/add/who
+printf "\n"
 curl -X POST http://localhost:7718/add/fought
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/have
+printf "\n"
 curl -X POST http://localhost:7718/add/thus
+printf "\n"
 curl -X POST http://localhost:7718/add/far
+printf "\n"
 curl -X POST http://localhost:7718/add/so
+printf "\n"
 curl -X POST http://localhost:7718/add/nobly
+printf "\n"
 curl -X POST http://localhost:7718/add/advanced
+printf "\n"
 curl -X POST http://localhost:7718/add/It
+printf "\n"
 curl -X POST http://localhost:7718/add/is
+printf "\n"
 curl -X POST http://localhost:7718/add/rather
+printf "\n"
 curl -X POST http://localhost:7718/add/for
+printf "\n"
 curl -X POST http://localhost:7718/add/us
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/be
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/dedicated
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/great
+printf "\n"
 curl -X POST http://localhost:7718/add/task
+printf "\n"
 curl -X POST http://localhost:7718/add/remaining
+printf "\n"
 curl -X POST http://localhost:7718/add/before
+printf "\n"
 curl -X POST http://localhost:7718/add/us
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/from
+printf "\n"
 curl -X POST http://localhost:7718/add/these
+printf "\n"
 curl -X POST http://localhost:7718/add/honored
+printf "\n"
 curl -X POST http://localhost:7718/add/dead
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/take
+printf "\n"
 curl -X POST http://localhost:7718/add/increased
+printf "\n"
 curl -X POST http://localhost:7718/add/devotion
+printf "\n"
 curl -X POST http://localhost:7718/add/to
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/cause
+printf "\n"
 curl -X POST http://localhost:7718/add/for
+printf "\n"
 curl -X POST http://localhost:7718/add/which
+printf "\n"
 curl -X POST http://localhost:7718/add/they
+printf "\n"
 curl -X POST http://localhost:7718/add/gave
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/last
+printf "\n"
 curl -X POST http://localhost:7718/add/full
+printf "\n"
 curl -X POST http://localhost:7718/add/measure
+printf "\n"
 curl -X POST http://localhost:7718/add/of
+printf "\n"
 curl -X POST http://localhost:7718/add/devotion
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/we
+printf "\n"
 curl -X POST http://localhost:7718/add/here
+printf "\n"
 curl -X POST http://localhost:7718/add/highly
+printf "\n"
 curl -X POST http://localhost:7718/add/resolve
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/these
+printf "\n"
 curl -X POST http://localhost:7718/add/dead
+printf "\n"
 curl -X POST http://localhost:7718/add/shall
+printf "\n"
 curl -X POST http://localhost:7718/add/not
+printf "\n"
 curl -X POST http://localhost:7718/add/have
+printf "\n"
 curl -X POST http://localhost:7718/add/died
+printf "\n"
 curl -X POST http://localhost:7718/add/in
+printf "\n"
 curl -X POST http://localhost:7718/add/vain
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/this
+printf "\n"
 curl -X POST http://localhost:7718/add/nation
+printf "\n"
 curl -X POST http://localhost:7718/add/under
+printf "\n"
 curl -X POST http://localhost:7718/add/God
+printf "\n"
 curl -X POST http://localhost:7718/add/shall
+printf "\n"
 curl -X POST http://localhost:7718/add/have
+printf "\n"
 curl -X POST http://localhost:7718/add/a
+printf "\n"
 curl -X POST http://localhost:7718/add/new
+printf "\n"
 curl -X POST http://localhost:7718/add/birth
+printf "\n"
 curl -X POST http://localhost:7718/add/of
+printf "\n"
 curl -X POST http://localhost:7718/add/freedom
+printf "\n"
 curl -X POST http://localhost:7718/add/and
+printf "\n"
 curl -X POST http://localhost:7718/add/that
+printf "\n"
 curl -X POST http://localhost:7718/add/government
+printf "\n"
 curl -X POST http://localhost:7718/add/of
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/people
+printf "\n"
 curl -X POST http://localhost:7718/add/by
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/people
+printf "\n"
 curl -X POST http://localhost:7718/add/for
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/people
+printf "\n"
 curl -X POST http://localhost:7718/add/shall
+printf "\n"
 curl -X POST http://localhost:7718/add/not
+printf "\n"
 curl -X POST http://localhost:7718/add/perish
+printf "\n"
 curl -X POST http://localhost:7718/add/from
+printf "\n"
 curl -X POST http://localhost:7718/add/the
+printf "\n"
 curl -X POST http://localhost:7718/add/earth
+printf "\n"
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	// We can do this natively just as easily, but this framework makes
@@ -15,21 +16,19 @@ const (
 )
 
 var (
-	beats, port              int
-	lc, uc, hash             chan string
-	item                     string
-	ucNotifier, hashNotifier chan bool
-	heartbeat                *time.Ticker
-	router                   *gin.Engine
+	beats, port                                     int
+	item                                            string
+	origAddress, lcAddress, titleAddress, ucAddress []string
+	lc, title, uc                                   chan string
+	heartbeat                                       *time.Ticker
+	router                                          *gin.Engine
 )
 
 // Do all of this stuff first.
 func init() {
-	ucNotifier = make(chan bool, 1)
-
 	lc = make(chan string, maxMessages)
 	uc = make(chan string, maxMessages)
-	hash = make(chan string, maxMessages)
+	title = make(chan string, maxMessages)
 	// In this example, we will hard code the port.  Later the environment
 	// will dictate.
 	port = 7718
@@ -41,8 +40,19 @@ func init() {
 	router = gin.New()
 
 	// These are the services we will be listening for.
+	// Insert words into the process. See gettysburg.sh for example.
 	router.POST("/add/:word", AddToLowerCaseQ)
+	// Show the total words processed by each stage (in real-time).
+	router.GET("/totals", GetTotals)
+	// Get the number of heartbeats put out by the application (also in real-time).
 	router.GET("/beats", GetHeartbeatCount)
+	// Show the results of the lowercase process.
+	router.GET("/lc", GetLCAddress)
+	// Show the results of the TitleCase process.
+	router.GET("/title", GetTitleAddress)
+	// Show the results of the UPPERCASE process.
+	router.GET("/uc", GetUCAddress)
+	// Make sure we are still alive.
 	router.GET("/ping", PingTheAPI)
 
 } // func
@@ -50,7 +60,32 @@ func init() {
 // AddToLowerCaseQ ...
 func AddToLowerCaseQ(c *gin.Context) {
 	lc <- c.Param("word")
-	content := gin.H{"payload": "Accepted: " + fmt.Sprintf("l: %d, U: %d, #: %d", len(lc), len(uc), len(hash))}
+	origAddress = append(origAddress, c.Param("word"))
+	content := gin.H{"payload": "Accepted: " + fmt.Sprintf("lc: %d, Tc: %d, UC: %d, #: %d", len(lc), len(title), len(uc), len(origAddress))}
+	c.JSON(http.StatusOK, content)
+}
+
+// GetTotals ...
+func GetTotals(c *gin.Context) {
+	content := gin.H{"payload": fmt.Sprintf("lc: %d, Tc: %d, UC: %d, #: %d", len(lcAddress), len(titleAddress), len(ucAddress), len(origAddress))}
+	c.JSON(http.StatusOK, content)
+}
+
+// GetLCAddress ...
+func GetLCAddress(c *gin.Context) {
+	content := gin.H{"payload": strings.Join(lcAddress, " ")}
+	c.JSON(http.StatusOK, content)
+}
+
+// GetTitleAddress ...
+func GetTitleAddress(c *gin.Context) {
+	content := gin.H{"payload": strings.Join(titleAddress, " ")}
+	c.JSON(http.StatusOK, content)
+}
+
+// GetUCAddress ...
+func GetUCAddress(c *gin.Context) {
+	content := gin.H{"payload": strings.Join(ucAddress, " ")}
 	c.JSON(http.StatusOK, content)
 }
 
@@ -78,47 +113,53 @@ func main() {
 			// When the Heartbeat ticker is fired, execute this.
 			case <-heartbeat.C:
 				beats++
-				fmt.Printf("bump,Bump... @ %s\n", time.Now().UTC())
+				fmt.Printf(`{"date":"%s","app":"gosp2","msgtype":"info","heartbeat":"%d"}`+"\n", time.Now().UTC(), beats)
+				//fmt.Printf("%s gosp2 msgtype=info heartbeat=%d\n", time.Now().UTC(), beats)
+				//fmt.Printf("bump,Bump... @ %s\n", time.Now().UTC())
 			} // select
 		} // for
 	}() // go func
 
-	// // Make lowercase.
-	// go func() {
-	// 	for {
-	// 		if len(lc) > 0 && (len(lc) < cap(lc)) {
-	// 			item = strings.ToLower(<-lc)
-	// 			fmt.Printf("%s\n", item)
-	// 			uc <- item
-	// 		} // if
-	// 	} // for
-	// }() // go func
+	// Make lowercase.
+	go func() {
+		for {
+			select {
+			case item := <-lc:
+				item = strings.ToLower(item)
+				lcAddress = append(lcAddress, item)
+				time.Sleep(time.Millisecond * 50)
+				title <- item
+			}
+		} // for
+	}() // go func
+
+	// // Make Title.
+	go func() {
+		for {
+			select {
+			case item = <-title:
+				item = strings.Title(item)
+				titleAddress = append(titleAddress, item)
+				time.Sleep(time.Millisecond * 100)
+				uc <- item
+			}
+		}
+	}()
 
 	// // Make UPPERCASE.
-	// go func() {
-	// 	for {
-	// 		if len(uc) > 0 && (len(uc) < cap(uc)) {
-	// 			item = strings.ToUpper(<-uc)
-	// 			fmt.Printf("%s\n", item)
-	// 			hash <- item
-	// 		} // if
-	// 	} // for
-	// }() // go func
-
-	// // Make hash.
-	// go func() {
-	// 	for {
-	// 		if len(hash) > 0 && (len(hash) < cap(hash)) {
-	// 			h := md5.New()
-	// 			h.Write([]byte(<-hash))
-	// 			fmt.Printf("%s\n", hex.EncodeToString(h.Sum(nil)))
-	// 		} // if
-	// 	}
-	// }()
+	go func() {
+		for {
+			select {
+			case item = <-uc:
+				item = strings.ToUpper(item)
+				ucAddress = append(ucAddress, item)
+			}
+		} // for
+	}() // go func
 
 	// After the 'go func' is dispatched, start the server and listen on the
 	// specified port.
-	fmt.Printf("ready on port %d\n", port)
+	fmt.Printf("%s gosp2 msgtype=info message=engine started, ready on port %d\n", time.Now().UTC(), port)
 	router.Run(":7718")
 	//router.Run(":" + strconv.Itoa(port))
 } // func
